bf266b46bc26906a2798cd8d04a0c27c9c6491f0
Ivan Prybolovetz <ivan.prybo@gmail.com>
2019-01-11 02:21:32 +0300

    first commit

--- PingPong/GameViewController.swift
+++ PingPong/GameViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
 
+var currentGameType = gameType.medium
+
+
+class GameViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +24,8 @@ class GameViewController: UIViewController {
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
+                
+                scene.size = view.bounds.size
                 
                 // Present the scene
                 view.presentScene(scene)
@@ -31,11 +37,11 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -43,7 +49,12 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
---
+++ PingPong/MenuViewController.swift
@@ -0,0 +1,101 @@
+//
+//  MenuViewController.swift
+//  PingPong
+//
+//  Created by Ivan on 1/11/19.
+//  Copyright © 2019 Ivan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+enum gameType {
+    case easy
+    case medium
+    case hard
+    case player2
+}
+
+class MenuViewController : UIViewController {
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    @IBOutlet   var buttons: [UIButton]!
+    
+    @IBAction func Player2(_ sender: UIButton) {
+        moveToGame(game: .player2)
+        sender.pulsate()
+    }
+    @IBAction func Easy(_ sender: Any) {
+        moveToGame(game: .easy)
+        
+    }
+    @IBAction func Medium(_ sender: Any) {
+        moveToGame(game: .medium)
+        
+    }
+    @IBAction func Hard(_ sender: Any) {
+        moveToGame(game: .hard)
+    }
+    
+    func moveToGame(game : gameType) {
+        let gameVC = self.storyboard?.instantiateViewController(withIdentifier: "gameVC") as! GameViewController
+        
+        currentGameType = game
+        
+        self.navigationController?.pushViewController(gameVC, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for button in buttons{
+            button.layer.cornerRadius = 25
+            button.layer.borderWidth = 6.0
+            button.layer.borderColor = (UIColor(red: 235.0/255.0, green: 133.0/255.0, blue: 137.0/255.0, alpha: 1.0)).cgColor
+            button.clipsToBounds = true
+        }
+        
+        do
+        {
+            let audioPath = Bundle.main.path(forResource: "Background", ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+        }
+        catch
+        {
+            //PROCESS ERROR
+        }
+        
+        let session = AVAudioSession.sharedInstance()
+        
+        do
+        {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        }
+        catch
+        {
+        }
+        
+        player.play()
+        
+    }
+}
+
+extension UIButton {
+    
+    func pulsate() {
+        
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.6
+        pulse.fromValue = 0.95
+        pulse.toValue = 1.0
+        pulse.autoreverses = true
+        pulse.repeatCount = 2
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        
+        layer.add(pulse, forKey: "pulse")
+    }
+}
