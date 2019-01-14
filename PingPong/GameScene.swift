b401286214b6653e220f1d4d107c88705de6e96f
Ivan Prybolovetz <ivan.prybo@gmail.com>
2019-01-14 16:49:09 +0300

    Second commit

--- PingPong/GameScene.swift
+++ PingPong/GameScene.swift
@@ -9,7 +9,9 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene  {
+//    ,SKPhysicsContactDelegate
+//    var starField: SKEmitterNode!
     
     var ball = SKSpriteNode()
     var enemy = SKSpriteNode()
@@ -23,11 +25,19 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         //The function is called from the start of the game.
         
+//        starField = SKEmitterNode(fileNamed: "Starfield")
+//        starField.position = CGPoint(x: 0, y: 1472)
+//        starField.advanceSimulationTime(10)
+//        self.addChild(starField)
+//        
+//        //Position on the background
+//        starField.zPosition = -1
+        
         topLbl = self.childNode(withName: "topLabel") as! SKLabelNode
         btmLbl = self.childNode(withName: "btmLabel") as! SKLabelNode
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         
-        print(self.view?.bounds.height)
+        print(self.view?.bounds.height as Any)
         
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
         enemy.position.y = (self.frame.height / 2) - 50
@@ -38,6 +48,7 @@ class GameScene: SKScene {
         let border  = SKPhysicsBody(edgeLoopFrom: self.frame)
         
         border.friction = 0
+        //beats off ball
         border.restitution = 1
         
         self.physicsBody = border
@@ -45,12 +56,37 @@ class GameScene: SKScene {
         startGame()
     }
     
+//    func didBegin(_ contact: SKPhysicsContact) {
+//        var alienBody: SKPhysicsBody
+//        var bulletBody: SKPhysicsBody
+//
+//        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask{
+//            alienBody = contact.bodyA
+//            bulletBody = contact.bodyB
+//        }else {
+//            alienBody = contact.bodyB
+//            bulletBody = contact.bodyA
+//        }
+//
+//
+//    }
+//    func collisionElements(bulletNode:SKSpriteNode, alienNode:SKSpriteNode ){
+//        let explosion = SKEmitterNode(fileNamed: "Vzriv")
+//        explosion?.position = alienNode.position
+//        self.addChild(explosion!)
+//
+//        self.run(SKAction.playSoundFileNamed("Vzriv.mp3", waitForCompletion: false))
+//        self.run(SKAction.wait(forDuration: 2)){
+//            explosion?.removeFromParent()
+//        }
+//    }
+    
     func startGame() {
         score = [0,0]
         topLbl.text = "\(score[1])"
         btmLbl.text = "\(score[0])"
         
-        ball.physicsBody?.applyImpulse(CGVector(dx: 30 , dy: 30))
+        ball.physicsBody?.applyImpulse(CGVector(dx: 37 , dy: 37))
         //The first movement of the ball.
     }
     
@@ -61,11 +97,11 @@ class GameScene: SKScene {
         
         if playerWhoWon == main {
             score[0] += 1
-            ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
+            ball.physicsBody?.applyImpulse(CGVector(dx: 37, dy: 37))
         }
         else if playerWhoWon == enemy {
             score[1] += 1
-            ball.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
+            ball.physicsBody?.applyImpulse(CGVector(dx: -37, dy: -37))
         }
         
         topLbl.text = "\(score[1])"
@@ -74,7 +110,7 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //Tap the screen
-        
+        //My movement
         for touch in touches {
             let location = touch.location(in: self)
             
@@ -98,6 +134,7 @@ class GameScene: SKScene {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         //We drive on the screen
+        //My movement
         
         
         for touch in touches {
@@ -125,15 +162,16 @@ class GameScene: SKScene {
         //The function is called every second.
         // Called before each frame is rendered
         
+        //Opponent's movement
         switch currentGameType {
         case .easy:
-            enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.3))
+            enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
             break
         case .medium:
-            enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.9))
+            enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.4))
             break
         case .hard:
-            enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
+            enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.14))
             break
         case .player2:
             
